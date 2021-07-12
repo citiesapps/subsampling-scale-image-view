a6a66b31ddde683955f8547a166b7dece1d49fa0
Trevor Rous <t.rous@citiesapps.com>
2021-07-12 11:06:02 +0200

    Change visibility

--- library/src/main/java/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
+++ library/src/main/java/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
@@ -2292,7 +2292,7 @@ public class SubsamplingScaleImageView extends View {
     /**
      * Adjust a requested scale to be within the allowed limits.
      */
-    private float limitedScale(float targetScale) {
+    protected float limitedScale(float targetScale) {
         targetScale = Math.max(minScale(), targetScale);
         targetScale = Math.min(maxScale, targetScale);
         return targetScale;
